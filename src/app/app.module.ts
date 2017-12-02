2c84e5285fb436e1874c062102a29a61311ae634
Piotr Podbielski <p.podbielski@gmail.com>
2017-12-02 15:38:38 +0100

    Dodano stronę główną i router.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {StronaGlownaComponent} from './strona-glowna/strona-glowna.component';
+import {RouterModule, Routes} from "@angular/router";
+import {MatCardModule, MatIconModule, MatTabsModule, MatToolbarModule} from "@angular/material";
+import {CovalentLayoutModule} from "@covalent/core";
 
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'powitanie', pathMatch: 'full' },
+  { path: 'powitanie',
+    component: StronaGlownaComponent,
+    data: { title: 'Fast Courier' }
+  },
+  { path: '**',
+    redirectTo: 'powitanie',
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StronaGlownaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatCardModule,
+    MatIconModule,
+    CovalentLayoutModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: false } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
