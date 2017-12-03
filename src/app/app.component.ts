9bde1c6eb2868a9c512edfd8a2ca742efea95169
adamexx <adam123adam@op.pl>
2017-12-03 16:38:47 +0100

    Dodanie formularza rejestracji do serwisu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,10 @@ export class AppComponent {
       {
         'label': 'Strona główna',
         'path': 'powitanie'
+      },
+      {
+        'label': 'Rejestracja',
+        'path': '/rejestracja'
       }
     ];
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,28 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {StronaGlownaComponent} from './strona-glowna/strona-glowna.component';
 import {RouterModule, Routes} from "@angular/router";
-import {MatCardModule, MatIconModule, MatTabsModule, MatToolbarModule} from "@angular/material";
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatTabsModule,
+  MatToolbarModule
+} from "@angular/material";
 import {CovalentLayoutModule} from "@covalent/core";
+import {RejestracjaComponent} from './rejestracja/rejestracja.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'powitanie', pathMatch: 'full' },
   { path: 'powitanie',
     component: StronaGlownaComponent,
     data: { title: 'Fast Courier' }
+  },
+  {
+    path: 'rejestracja',
+    component: RejestracjaComponent,
+    data: { title: 'Fast Courier - Rejestracja' }
   },
   { path: '**',
     redirectTo: 'powitanie',
@@ -22,7 +36,8 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    StronaGlownaComponent
+    StronaGlownaComponent,
+    RejestracjaComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +46,9 @@ const appRoutes: Routes = [
     MatCardModule,
     MatIconModule,
     CovalentLayoutModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: false } // <-- debugging purposes only
