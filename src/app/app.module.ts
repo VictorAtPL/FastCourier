4c1202a741ae8d9b577e1f7f849d4cfe79b5e79c
SirMich <murzyn66@gmail.com>
2017-12-03 20:29:31 +0100

    Dodanie podstrony wyszukiwania

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,32 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {StronaGlownaComponent} from './strona-glowna/strona-glowna.component';
 import {RouterModule, Routes} from "@angular/router";
-import {MatCardModule, MatIconModule, MatTabsModule, MatToolbarModule} from "@angular/material";
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatDatepickerModule,
+  MatIconModule,
+  MatInputModule,
+  MatNativeDateModule,
+  MatSelectModule,
+  MatTabsModule,
+  MatToolbarModule
+} from "@angular/material";
 import {CovalentLayoutModule} from "@covalent/core";
+import {WyszukiwanieComponent} from './wyszukiwanie/wyszukiwanie.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'powitanie', pathMatch: 'full' },
   { path: 'powitanie',
     component: StronaGlownaComponent,
     data: { title: 'Fast Courier' }
+  },
+  {
+    path: 'StronaGlowna/Wyszukiwanie',
+    component: WyszukiwanieComponent,
+    data: { title: 'Fast Courier - Wyszukiwanie' }
   },
   { path: '**',
     redirectTo: 'powitanie',
@@ -22,7 +40,8 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    StronaGlownaComponent
+    StronaGlownaComponent,
+    WyszukiwanieComponent
   ],
   imports: [
     BrowserModule,
@@ -31,6 +50,13 @@ const appRoutes: Routes = [
     MatCardModule,
     MatIconModule,
     CovalentLayoutModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    BrowserAnimationsModule,
+    MatNativeDateModule,
+    MatButtonModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: false } // <-- debugging purposes only
