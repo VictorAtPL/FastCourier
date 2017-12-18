2360ebcc06ea8380dcc7fde1a9d20466ee4b2eb6
VictorAtPL <p.podbielski@gmail.com>
2017-12-18 15:21:05 +0100

    UC: Przejrzyj dostepne oferty - Dodanie komentarzy w celu wygenerowania dokumentacji Typedoc.

--- src/app/components/przejrzyj-dostepne-oferty/przejrzyj-dostepne-oferty.component.ts
+++ src/app/components/przejrzyj-dostepne-oferty/przejrzyj-dostepne-oferty.component.ts
@@ -3,20 +3,77 @@ import {OfertaService} from '../../services/oferta.service';
 import {MatTableDataSource} from '@angular/material';
 import {DataSource} from '@angular/cdk/collections';
 
+/**
+ * Interfejs definiujacy obiekt oferta, który jest wyświetlany w tabeli podglądu ofert.
+ * @author Piotr Podbielski
+ * @since 0.0.3
+ * @copyright Magical Solutions
+ * @license Creative Commons Attribution NonCommercial (CC-BY-NC)
+ */
 export interface Oferta {
+  /**
+   * Id oferty.
+   */
   id: number;
+
+  /**
+   * Tytuł oferty.
+   */
   tytul: string;
+
+  /**
+   * Miasto docelowe.
+   */
   miastoDocelowe: string;
+
+  /**
+   * Cena maksymalna, której zażąda wystawiający.
+   */
   cenaMaksymalna: number;
+
+  /**
+   * Login użytkownika wystawiającego ofertę.
+   */
   loginUzytkownika: string;
+
+  /**
+   * Tablica z kategoriami paczek, które może przewieźć wystawiający.
+   */
   kategoriePaczek: string[];
+
+  /**
+   * Tablica z rozmiarami paczek, które może przewieźć wystawiający.
+   */
   rozmiaryPaczek: string[];
+
+  /**
+   * Maksymalna waga paczki, którą może przewieźć wystawiający.
+   */
   maksymalnaWagaPaczki: null | number;
+
+  /**
+   * Miasto początkowe z którego wyjeżdza wystawiający.
+   */
   miastoPoczatkowe: string;
+
+  /**
+   * Cena minimalna, której zażąda wystawiający.
+   */
   cenaMinimalna: number;
+
+  /**
+   * Data i godzina wyjazdu wystawiającego.
+   */
   dataGodzinaWyjazdu: Date;
 }
 
+/**
+ * Definicja komponentu PrzejrzyjDostepneOferty, ktory odpowiada za wyswietlenie strony z tabela w ktorej znajduje sie lista ofert.
+ * @author Piotr Podbielski
+ * @since 0.0.3
+ * @copyright Magical Solutions
+ * @license Creative Commons Attribution NonCommercial (CC-BY-NC)
+ */
 @Component({
   selector: 'app-przejrzyj-dostepne-oferty',
   templateUrl: './przejrzyj-dostepne-oferty.component.html',
@@ -24,13 +81,31 @@ export interface Oferta {
   viewProviders: [OfertaService]
 })
 export class PrzejrzyjDostepneOfertyComponent implements OnInit {
+  /**
+   * Lista kolumn, które mają być wyświetlone w tabeli.
+   * @type {[string , string , string , string , string , string , string]}
+   */
   displayedColumns = ['dataGodzina', 'tytul', 'wystawiajacy', 'cena', 'kategoriePaczek', 'rozmiaryPaczek', 'maksymalnaWagaPaczki'];
+  /**
+   * Źródło danych dla komponentu tabeli.
+   */
   dataSource: DataSource<Oferta>;
+  /**
+   * Tablica z ofertami pobranymi z serwisu REST.
+   * @type {Array}
+   */
   data: Oferta[] = [];
 
+  /**
+   * Konstruktor wstrzykujący usługi z których można potem korzystać w komponencie.
+   * @param {OfertaService} ofertaService Komponent dostępu do ofert przez REST.
+   */
   constructor(private ofertaService: OfertaService) {
   }
 
+  /**
+   * Funkcja wywoływana przy inicjalizacji komponentu (ładowaniu strony przejrzenia dostępnych ofert).
+   */
   ngOnInit() {
     this.ofertaService.getOferty().subscribe(result => {
       const oferty: any[] = result._embedded.ofertas;
@@ -55,6 +130,12 @@ export class PrzejrzyjDostepneOfertyComponent implements OnInit {
     });
   }
 
+  /**
+   * Funkcja konwertująca ciągi znaków daty wyjazdu i godziny wyjazdu w obiekt JavaScript typu Date
+   * @param {string} dataWyjazdu
+   * @param {string} godzinaWyjazdu
+   * @returns {Date}
+   */
   private fetchDataGodzinaWyjazdu(dataWyjazdu: string, godzinaWyjazdu: string): Date {
     const godzinaWyjazdu_a = godzinaWyjazdu.split(':');
     return new Date(new Date(dataWyjazdu).setHours(Number(godzinaWyjazdu_a[0]), Number(godzinaWyjazdu_a[1]), 0));
--- src/app/services/oferta.service.ts
+++ src/app/services/oferta.service.ts
@@ -2,32 +2,73 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 
+/**
+ * Klasa usługi odpowiedzialnej za kontakt z serwisem REST w dziedzinie ofert
+ */
 @Injectable()
 export class OfertaService {
 
+  /**
+   * Konstruktor wstrzykujący usługi z których można potem korzystać w komponencie.
+   * @param {HttpClient} http
+   */
   constructor(private http: HttpClient) {
   }
 
+  /**
+   *
+   * @author Przemysław Proczek
+   * @param data
+   * @returns {Observable<Object>}
+   */
   postOferta(data: any) {
     return this.http.post<any>(environment.restUrl + '/oferty', data);
   }
 
+  /**
+   *
+   * @author Adrian Plichta
+   * @param {number} id
+   * @returns {Observable<Object>}
+   */
   getOferta(id: number) {
     return this.http.get<any>(environment.restUrl + '/oferty/' + id);
   }
 
+  /**
+   *
+   * @author Michał Pruchniewski
+   * @param data
+   * @returns {Observable<Object>}
+   */
   postZgloszenieOferty(data: any) {
     return this.http.post<any>(environment.restUrl + '/zgloszeniaofert', data);
   }
 
+  /**
+   *
+   * @author Michał Pruchniewski
+   * @param {string} url
+   * @param data
+   * @returns {Observable<Object>}
+   */
   putZgloszonaOfertaZgloszenieOfert(url: string, data: any) {
     return this.http.put<any>(url, data, {headers: {'Content-Type': 'text/uri-list'}});
   }
 
+  /**
+   *
+   * @author Michał Pruchniewski
+   * @returns {Observable<Object>}
+   */
   getZgloszeniaOfert() {
     return this.http.get<any>(environment.restUrl + '/zgloszeniaofert?projection=verbose');
   }
 
+  /**
+   * @author Piotr Podbielski
+   * @returns {Observable<Object>} Obiekt typu observable zwracający wynik wykonania żądania HTTP, które pobiera listę ofert przez REST.
+   */
   getOferty() {
     return this.http.get<any>(environment.restUrl + '/oferty?projection=verbose');
   }
