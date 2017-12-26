c15c6488e7368687ada5deda4803a2d41edec647
NieMamPojeciaCoRobie <marcinmarcinkowskioff@gmail.com>
2017-12-26 19:53:48 +0100

    Dodanie walidacji do formularza UC: Zglos Transakcje.

--- src/app/components/zglaszanie-transakcji/zglaszanie-transakcji.component.ts
+++ src/app/components/zglaszanie-transakcji/zglaszanie-transakcji.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 /**
  * Logika biznesowa dla komponentu zgłaszania transakcji
@@ -8,6 +9,31 @@ import {Component} from '@angular/core';
   templateUrl: './zglaszanie-transakcji.component.html',
   styleUrls: ['./zglaszanie-transakcji.component.css']
 })
-export class ZglaszanieTransakcjiComponent {
+export class ZglaszanieTransakcjiComponent implements OnInit, OnDestroy {
   selected: String;
+
+  zglosTransakcjeForm: FormGroup;
+
+  public powody: any[] = [{value: 'Niezastosowanie się do warunków transakcji przez Kierowcę'},
+    {value: 'Niezastosowanie się do warunków transakcji przez Nadawcę'},
+    {value: 'Inne'}];
+  private sub: any;
+
+  ngOnInit(): void {
+    this.zglosTransakcjeForm = new FormGroup({
+      'powod': new FormControl('', [Validators.required]),
+      'tresc': new FormControl('', [Validators.required, Validators.minLength(40), Validators.maxLength(500)])
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
+
+  zglos() { // dodałem tylko szkielet. Walidacja zrobiona, ale brak logiki ~Marcinexxx.
+    const data = {
+      powod: this.zglosTransakcjeForm.controls['powod'].value.value,
+      tresc: this.zglosTransakcjeForm.controls['tresc'].value
+    };
+  }
 }
